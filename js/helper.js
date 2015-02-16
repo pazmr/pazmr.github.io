44d0ffa14346a1da359b334e3dc39d00d3286adf
Max Paz <maxpaz2004@gmail.com>
2015-02-16 12:21:56 -0400

    Mayor
    Mayor changes

--- js/helper.js
+++ js/helper.js
@@ -17,13 +17,15 @@ var HTMLheaderRole = '<span>%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><a id = "mailto" href = "mailto:%data%">%data%</a></li>';
+var HTMLtwitter = '<a class="icon" href="%data%" target="_blank"><img class="logoPic" src="/images/Twitter_logo_blue.png"></a>';
+var HTMLlinkedin = '<a class="icon" href="%data%" target="_blank"><img class="logoPic" src="/images/linkedin_logo.png"></a>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLfaceIco = '<a class="icon" href="%data%" target="_blank"><img class="logoPic" src="/images/facebook.png"></a>';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
@@ -43,7 +45,7 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a href="#" target="_blank">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
@@ -54,6 +56,7 @@ var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
+
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,8 @@
 
 var bio = {"name": 'Maximiliano Paz', "role": "Budget Analyst",
             "contacts":  {"mobile": "(591)-75851523", "email" : "maxpaz2004@hotmail.com",
-            "location": "Av. Desaguadero, La Paz,bolivia"}, "picture": "images/myphoto.jpg", "welcome": "Welcome, this is my updated CV",
+            "location": "Desaguadero Ave, La Paz,bolivia"}, "picture": "images/myphoto.jpg", "welcome": "Welcome, this is my updated CV", "facebook": "https://www.facebook.com/maximiliano.r.paz", "twitter": "https://twitter.com/MaximilianoPaz1",
+            "linkedin": "https://www.linkedin.com/in/maximimilianopaz",
             "skills": ["Finance","Accounting", "Budgeting","Financial modeling", "Data Analysis","Programming","Microsoft Office","Visual Basic", "Python", "Javascript", "R language", "SQL" ] };
 var work = {"jobs":[
     {
@@ -9,28 +10,44 @@ var work = {"jobs":[
         "title": "Budget Analist",
         "location": "Av. 6 de Agosto, La Paz, Bolivia",
         "dates": "6/01/2014-Current",
-        "description": "Provides full financial services for OBO (Overrseas Building Operations) and accounting services for PD (Public Diplomacy) and APHIS (Animal Plant Health Inspection Services). Prepares financial reports for PD and records obligations in RFMS. Serves as back-up to the "
+        "description": "Provides full financial services for OBO (Overseas Building Operations) and accounting services for PD (Public Diplomacy) and APHIS (Animal Plant Health Inspection Services). Prepares financial reports for PD and records obligations in RFMS. Serves as back-up to the "
 + " budget analysts for ICASS, Program, Rep, MSG, DS and DAO allotments. Manages the post-specific software eFMO and Utility Billing System. Inputs all data and manages all user information for both systems, monitors data integrity, and makes corrections."    },
     {
         "employer": "American Embassy - La Paz",
         "title": "Travel Voucher Examiner",
         "location": "Av. 6 de Agosto, La Paz, Bolivia",
         "dates": "9/15/2009-5/31/2014",
-        "description": "Coordinate travel arrangements for the embassy staff"
+        "description": "Under the supervision of the Financial Assistant/Payments supervisor, incumbent prepares travel vouchers reimbursements and travel advances, based on traveler’s supporting documents, US employees, FSNs or grantees invited by U.S. Government (USG). " + 
+"Ensures that all travel expenses are in accordance with the Department of State (DOS) travel regulation policies and procedures. Answers travel-reimbursement related questions raised by official travelers at post and from visitors. Incumbent calculates travel reimbursements for all projects personnel. Incumbent reconciles accounts from the credit card and processes them for final payment. " +
+"The incumbent pays approximately US$715,000.00 annually. Incumbent is a Specialist in creating financial reports extracted from the COAST and LFMS both financial programs requiered by INL and American managers in Bolivia."
+        
     },
     {
         "employer": "Banco Union S.A.",
         "title": "Internal Auditor",
         "location": "Av. Camacho, La Paz, Bolivia",
         "dates": "6/1/2009-8/30/2014",
-        "description": "Coordinate and perform financial audits"
+        "description": "Under direction of Internal Audit Manager, responsible for the timely execution of risk-based internal audits in accordance with the annual audit plan, as well as assisting with other audit matters and projects. " + 
+        "Conduct risk assessment of assigned department or functional area in established/required timeline. " + 
+        "Establish risk-based audit programs. Determine scope of review in conjunction with the Audit Manager. Review the suitability of internal control design. Conduct audit testing of specified area and identify reportable issues and dimension of risk." + 
+        "Determine compliance with policies and procedures. Verbally communicate findings to senior management and draft comprehensive and complete report of audit area."
     },
     {
-        "employer": "Erns & Young - Bolivia",
-        "title": "Auditor - Assurance Advisory Business Services ",
+        "employer": "Ernst & Young - Bolivia",
+        "title": "Senior Auditor - Assurance Advisory Business Services ",
         "location": "Av. Campos, La Paz, Bolivia",
-        "dates": "3/1/2007-5/30/2009",
-        "description": "Coordinate and perform financial audits on private companies"
+        "dates": "2/1/2008-5/30/2009",
+        "description": "Ensures successful completion of assigned audit engagements, from start to finish, inclusive of preplanning and wrap up activities. Depending on assigned engagement, reports to one or more member(s) of the Audit Managers. " + 
+        " Manages and directs daily activities of more junior auditors assigned to assist the Supervising Auditor during an engagement. Applies risk and control concepts to scenarios encountered and identifies any potential issues. " + 
+        "Communicates identified issues with the Audit Managers to ensure any potential concerns are addressed in a timely and effective manner. Delivers the final audit report to the audit managers, and elaborates the internal control report for corrective actions."
+    },
+    {
+        "employer": "Ernst & Young - Bolivia",
+        "title": "Junior Auditor - Assurance Advisory Business Services ",
+        "location": "Av. Campos, La Paz, Bolivia",
+        "dates": "3/1/2007-1/31/2008",
+        "description": "Collaborates in planning audits, field work for internal control evaluation, audit tests, confirmation with customers and report preparation, and performing periodic office and administrative duties." + 
+        " Reviews documents and composing summary memos, identify, develop, and document audit issues and assist in the preparation of the draft audit report."
     }
     ],
     "display": function (){
@@ -54,11 +71,30 @@ var work = {"jobs":[
         }
 };
 var projects = {
+    
         "projects":[
             {
-                "title": "None",
-                "dates": "None",
-                "description": "None",
+                "title": "Stock Market simulator (personal project)",
+                "dates": "2015",
+                "description": "Python script, automatically gets S&P500 tickers from wikipedia, with this, the script obtains historic prices for each stock from yahoo finance, performs a series of market simulations based on custom estratefies and creates buy/sell orders on a simulated protfolio.",
+                "images": []
+            },
+            {
+                "title": "AFP payment builder",
+                "dates": "2014",
+                "description": "Excel Macro, automatically extracts biweekly zipped payroll files, then combines and formats them to present them as the required forms for social security payments. ",
+                "images": []
+            },
+            {
+                "title": "Payroll by Agencies",
+                "dates": "2014",
+                "description": "Excel Macro, automatically splits raw payroll files by agency and archive them into separate files for use of the different agencies the embassy has",
+                "images": []
+            },
+            {
+                "title": "INL Financial reporter",
+                "dates": "2012",
+                "description": "Access application, automatically uploads data from official financial system and performs a series of operations and outputs excel reports for management decision taking",
                 "images": []
             }
             ],
@@ -89,6 +125,7 @@ var projects = {
         }
         }
 };
+
 var education = {
         "schools": [
             {
@@ -145,7 +182,7 @@ var education = {
           
                     for (sch in education.schools){
                         $("#education").append(HTMLschoolStart);
-                        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[sch].name);
+                        var formattedSchoolName = HTMLschoolName.replace("#", education.schools[sch].onlineInfo).replace("%data%", education.schools[sch].name);
                         var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[sch].degree);
                         var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[sch].dates);
                         var formattedSchoollocation = HTMLschoolLocation.replace("%data%", education.schools[sch].location);
